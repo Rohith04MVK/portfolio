ec288ce9bfe904aeb8ac891d3cfcea5bcc13ec0b
Rohith04MVK <007rohitjj04@gmail.com>
2022-09-11 19:56:18 +0530

    Added new favicon icon and fixed fonts

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,7 +9,7 @@ const Home = () => {
       <div className="inset-0 bg-black opacity-25 absolute"></div>
       <div className="container mx-auto px-6 md:px-12 relative z-10 flex items-center py-32 xl:py-40">
         <div className="lg:w-3/5 xl:w-2/5 flex flex-col items-start relative z-10">
-          <span className="font-cartoon font-bold uppercase text-pink text-6xl">
+          <span className="font-cartoon font-bold uppercase text-pink text-8xl">
             Hi, I'm Rohith
           </span>
           <h1 className="font-robo font-bold text-4xl sm:text-4xl text-white leading-tight mt-4 text-left">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {}
+    fontFamily: {
+      'nice-fonts': ['JetBrains Mono'],
+      'robo': ['Roboto'],
+      'cartoon': ['Mochiy Pop One']
+    },
+    colors: {
+      'background': '#30304B',
+      'black': '#1B1F23',
+      'text': '#CEB5CC',
+      'gray': '#A29587',
+      'white':'#deeafa',
+      'blue': '#A3CBE7',
+      'light-gray': '#9C9C9C',
+      'pink':'#FBD2D1',
+      'button-red':'#f56565',
+      'button-green':'#68d391',
+      'button-yellow':'#faf089',
+    },
+
+    extend: {
+      backgroundImage: {
+        'backgorund-img': "url('/public/background.png')",
+     },
+    },
   },
-  plugins: []
-};
+  plugins: [],
+}
