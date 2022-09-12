fa1096d5ec330e8d2c28969eace940097e8a62e8
Rohith04MVK <007rohitjj04@gmail.com>
2022-09-12 20:07:39 +0530

    Added new favicon icon and fixed fonts

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,25 @@
 const Home = () => {
   return (
-    <div className="bg-indigo-900 relative overflow-hidden h-screen">
-      <img
-        src="https://wallpaperaccess.com/full/6836716.png"
-        className="absolute h-full w-full object-cover"
-      />
-
-      <div className="inset-0 bg-black opacity-25 absolute"></div>
-      <div className="container mx-auto px-6 md:px-12 relative z-10 flex items-center py-32 xl:py-40">
-        <div className="lg:w-3/5 xl:w-2/5 flex flex-col items-start relative z-10">
-          <span className="font-cartoon font-bold uppercase text-pink text-8xl">
-            Hi, I'm Rohith
-          </span>
-          <h1 className="font-robo font-bold text-4xl sm:text-4xl text-white leading-tight mt-4 text-left">
-            Machine learning & <br /> Backend engineer
-          </h1>
+        <div className="bg-indigo-900 relative overflow-hidden h-screen">
+            <img src="https://wallpaperaccess.com/full/6836716.png" className="absolute h-full w-full object-cover" />
+            <div className="inset-0 bg-black opacity-25 absolute">
+            </div>
+            <div className="container mx-auto px-6 md:px-12 relative z-10 flex items-center py-32 xl:py-40">
+                <div className="lg:w-3/5 xl:w-2/5 flex flex-col items-start relative z-10">
+                    <span className="font-cartoon font-bold uppercase text-pink text-6xl">
+                        Hi, I'm Rohith
+                    </span>
+                    <h1 className="font-robo font-bold text-6xl sm:text-7xl text-white leading-tight mt-4 text-left">
+                        Machine learning &
+                        <br />
+                        Backend engineer
+                    </h1>
+                    <a href="#" className="block bg-background hover:bg-gray py-3 px-4 rounded-lg text-lg text-gray-800 font-bold uppercase mt-10 text-pink">
+                        Discover
+                    </a>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
   );
 };
 
