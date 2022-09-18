dc83e9708782995b6ccf40a39dd41c3d71657989
Rohith04MVK <007rohitjj04@gmail.com>
2022-09-18 20:40:39 +0530

    Added better cards

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,19 +4,25 @@ interface CardProps {
   src: string;
   title: string;
   children: ReactNode;
+  tag1: string;
+  tag2: string;
+  tag3: string;
 }
 
-const Card = ({ src, title, children }: CardProps) => {
+const Card = ({ src, title, children, tag1, tag2, tag3}: CardProps) => {
   return (
-    <div className="flex justify-between pt-10 px-5">
-      <div className="rounded-lg shadow-lg bg-white max-w-sm transform transition-all opacity-50 hover:scale-105">
-        <a href="#!">
-          <img className="rounded-t-lg" src={src} alt="" />
-        </a>
-        <div className="p-6">
-          <h5 className="text-text text-xl font-medium mb-2">{title}</h5>
-          <p className="text-text text-base mb-4">{children}</p>
-        </div>
+    <div className="rounded overflow-hidden shadow-lg">
+      <img className="w-full" src={src} alt="Mountain" />
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{title}</div>
+        <p className="text-gray-700 text-base">
+          {children}
+        </p>
+      </div>
+      <div className="px-6 pt-4 pb-2">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#{tag1}</span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#{tag2}</span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#{tag3}</span>
       </div>
     </div>
   );
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -2,30 +2,10 @@ import Card from '../components/Card';
 
 const Projects = () => {
   return (
-    <div className="bg-indigo-900 relative overflow-hidden h-screen">
-      <img
-        src="https://wallpapercave.com/wp/wp7563482.jpg"
-        className="absolute h-full w-full object-cover"
-      />
-      <div className="grid lg:grid-cols-3">
-        <Card
-          title="Brain Tumor Detection Using UNET"
-          src="https://prod-images-static.radiopaedia.org/images/17420182/258f5e41fb90d89f73d24c077e93f5_big_gallery.jpeg"
-        >
-          Hello Noobs
-        </Card>
-        <Card
-          title="Neural style transfer"
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/1200px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg"
-        >
-          Test
-        </Card>
-        <Card
-          title="Recolor"
-          src="https://github.com/Rohith04MVK/Recolor/blob/main/images/index.jpg?raw=true"
-        >
-          Test
-        </Card>
+    <div>
+      <img src="https://wallpapercave.com/wp/wp7563482.jpg" className="absolute h-full w-full object-cover z-0" />
+      <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5 z-10">
+        <Card title='Test' src="https://upload.wikimedia.org/wikipedia/commons/e/e7/Everest_North_Face_toward_Base_Camp_Tibet_Luca_Galuzzi_2006.jpg" tag1="test" tag2="test" tag3="test">This is a test</Card>
       </div>
     </div>
   );
